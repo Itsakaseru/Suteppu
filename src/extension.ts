8f72b9b97dfaeb1b8a594e87396b88fccbf7971e
Lemuel Lancaster <32617101+Itsakaseru@users.noreply.github.com>
2021-08-21 20:58:58 +0700

    Hooking all commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,36 @@
 import * as vscode from "vscode";
 import saveStep from "./commands/saveStep";
 import redoStep from "./commands/redoStep";
+import saveToFile from "./commands/saveToFile";
+import loadFromFile from "./commands/loadFromFile";
+import { SuteppuProvider } from "./provider/SuteppuProvider";
 
 export function activate(context: vscode.ExtensionContext)
 {
-	console.log('[Suteppu] is now active!');
+	console.log("[Suteppu] is now active!");
 
 	const temporaryStorage: any = [];
 
-	let cmdSaveStep = vscode.commands.registerCommand('suteppu.saveStep', () => saveStep(temporaryStorage));
-	let cmdRedoStep = vscode.commands.registerCommand('suteppu.reStep', () => redoStep(temporaryStorage));
+	const suteppu = new SuteppuProvider(temporaryStorage);
+
+	vscode.window.registerTreeDataProvider(
+		"suteppu-storage",
+		suteppu
+	);
+
+	vscode.window.createTreeView("suteppu-storage", {
+		treeDataProvider: suteppu
+	});
+
+	let cmdSaveStep = vscode.commands.registerCommand("suteppu.saveStep", () => saveStep(suteppu, temporaryStorage));
+	let cmdRedoStep = vscode.commands.registerCommand("suteppu.reStep", () => redoStep(suteppu, temporaryStorage));
+	let cmdSaveToFile = vscode.commands.registerCommand("suteppu.saveToFile", () => saveToFile(temporaryStorage));
+	let cmdLoadFromFile = vscode.commands.registerCommand("suteppu.loadFromFile", () => loadFromFile(suteppu, temporaryStorage));
 
 	context.subscriptions.push(cmdSaveStep);
 	context.subscriptions.push(cmdRedoStep);
+	context.subscriptions.push(cmdSaveToFile);
+	context.subscriptions.push(cmdLoadFromFile);
 }
 
 // this method is called when your extension is deactivated
